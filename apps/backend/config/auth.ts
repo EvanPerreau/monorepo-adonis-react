5ab7e3f01926a34518d754487358481dc6957c25
EvanPerreau <evan.perreau@gmail.com>
2024-07-09 13:50:23 +0200

    apply linter and adding few npm commands

--- apps/backend/config/auth.ts
+++ apps/backend/config/auth.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-shadow */
 import { defineConfig } from '@adonisjs/auth'
 import { tokensGuard, tokensUserProvider } from '@adonisjs/auth/access_tokens'
-import type { InferAuthEvents, Authenticators } from '@adonisjs/auth/types'
+import type { Authenticators, InferAuthenticators, InferAuthEvents } from '@adonisjs/auth/types'
 
 const authConfig = defineConfig({
   default: 'api',
@@ -8,7 +9,7 @@ const authConfig = defineConfig({
     api: tokensGuard({
       provider: tokensUserProvider({
         tokens: 'accessTokens',
-        model: () => import('#models/user')
+        model: () => import('#models/user'),
       }),
     }),
   },
@@ -20,9 +21,11 @@ export default authConfig
  * Inferring types from the configured auth
  * guards.
  */
+/* eslint-disable @typescript-eslint/no-shadow */
 declare module '@adonisjs/auth/types' {
   interface Authenticators extends InferAuthenticators<typeof authConfig> {}
 }
 declare module '@adonisjs/core/types' {
   interface EventsList extends InferAuthEvents<Authenticators> {}
 }
+/* eslint-enable @typescript-eslint/no-shadow */
